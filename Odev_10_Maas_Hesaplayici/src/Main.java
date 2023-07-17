a5874e29eb59cebac1fbe5a5a99a59dc4d0a7c06
Burak Tekeli <burak01tekeli@gmail.com>
2023-07-18 01:20:30 +0300

    Main.java projesi oluşturuldu.

---
+++ Odev_10_Maas_Hesaplayici/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee e1 = new Employee("kemal",2000,45,1985);
+        e1.tooString();
+        Employee e2 = new Employee("mahmut",-2500,-50,-2011);
+        e2.tooString();
+    }
+}
